8191cd5d1a7e6f4ca526207392e79194e0ef53ab
Jesus Sanchez <113047026+JesusSanchezM@users.noreply.github.com>
2023-12-03 14:49:24 -0600

    graficas de percentiles de ingresos, diferencias y tasas

--- Code file.R
+++ Code file.R
@@ -32,6 +32,9 @@ enigh_2022_ingresos %>% select(ing_tri) %>% summary()
 enigh_2022_ingresos %>% select(ing_tri) %>% summary()
 enigh_2022_ingresos %>% select(ing_tri) %>% summary()
 
+#----------------------
+#Analisis de datos
+#______________________
 
 # Creación del data frame con quantiles
 percentiles_data <- data.frame(
@@ -42,6 +45,8 @@ percentiles_data <- data.frame(
   value_2016 = quantile(enigh_2016_ingresos$ing_tri, prob = seq(0, 1, length = 101), na.rm = TRUE)
 )
 
+#Ingresos por año
+{
 percentiles_data %>% filter(percentile>0.01 & percentile<0.99) %>% 
   ggplot() + 
   geom_line(aes(x = percentile, y = value_2022), color = "blue") +
@@ -49,12 +54,63 @@ percentiles_data %>% filter(percentile>0.01 & percentile<0.99) %>%
   geom_line(aes(x = percentile, y = value_2018), color = "orange")+
   geom_line(aes(x = percentile, y = value_2016), color = "red")+
   theme_classic()
-
-percentiles_data %>% filter(percentile>0.01 & percentile<0.99) %>% 
-  mutate(diferencia=value_2022-value_2020) %>% 
-  ggplot() + 
-  geom_line(aes(x = percentile, y = diferencia), color = "blue") +
+}
+  
+#Diferencia de ingresos 
+{
+  
+percentiles_data  %>%
+  filter(percentile >= 0.01 & percentile <= 0.99) %>% 
+  mutate(
+    diferencia_2022_2020 = value_2022 - value_2020,
+    diferencia_2020_2018 = value_2020 - value_2018,
+    diferencia_2018_2016 = value_2018 - value_2016,
+    diferencia_2022_2016 = value_2022 - value_2016
+  ) %>% 
+  pivot_longer(
+    cols = starts_with("diferencia_"),
+    names_to = "linea",
+    values_to = "diferencia"
+  ) %>% 
+ggplot(aes(x = percentile, y = diferencia, color = linea)) +
+  geom_line(size = 1) +
+  labs(
+    title = "Diferencias entre Percentiles de Ingresos en Diferentes Años",
+    x = "Percentil",
+    y = "Diferencia",
+    color = "Línea"
+  ) +
   theme_classic()
+
+}
+
+#Tasa de crecimiento de ingresos 
+{
+percentiles_data  %>%
+  filter(percentile >= 0.01 & percentile <= 0.99) %>% 
+  mutate(
+    diferencia_2022_2020 = ((value_2022/value_2020)-1)*100,
+    diferencia_2020_2018 = ((value_2020 / value_2018)-1)*100,
+    diferencia_2018_2016 = ((value_2018 / value_2016)-1)*100,
+    diferencia_2022_2016 = ((value_2022 / value_2016)-1)*100
+  ) %>% 
+  pivot_longer(
+    cols = starts_with("diferencia_"),
+    names_to = "linea",
+    values_to = "diferencia"
+  ) %>% 
+  ggplot(aes(x = percentile, y = diferencia, color = linea)) +
+  geom_line(size = 1) +
+  labs(
+    title = "Diferencias entre Percentiles de Ingresos en Diferentes Años",
+    x = "Percentil",
+    y = "Diferencia",
+    color = "Línea"
+  ) +
+  theme_classic()
+}
+
+
 
 
 
